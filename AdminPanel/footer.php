95ef3709f5a5c9f19af05ae9268963e0d606e618
emhyripek <emiripek@hotmail.com.tr>
2025-06-01 21:02:32 +0300

    Update footer.php

--- AdminPanel/footer.php
+++ AdminPanel/footer.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-12">
         <div class="copyright">
-            <p>Copyright © 2024. Tüm hakları saklıdır.</p>
+            <p>Copyright © 2025. Tüm hakları saklıdır.</p>
         </div>
     </div>
 </div>
